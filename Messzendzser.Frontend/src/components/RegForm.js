8b93598f1143080f95857e73cc7451f774fc5d8c
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2022-09-23 15:19:03 +0200

    ajax_register

--- Messzendzser.Frontend/src/components/RegForm.js
+++ Messzendzser.Frontend/src/components/RegForm.js
@@ -3,27 +3,60 @@ import{useForm} from 'react-hook-form';
 
 export default function Form(){
 
-    const{register, handleSubmit, formState:{errors}}= useForm()
-    const onSubmit = data => console.log(data);
+    const{register,formState:{errors}}= useForm()
+    //const onSubmit = data => console.log(data);
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    
+
+    let handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+          let res = await fetch("http://localhost:7043/api/Register", {
+            method: "POST",
+            body: JSON.stringify({
+                username: username,
+                email: email,
+                password: password,
+            }),
+          });
+          let resJson = await res.json();
+          if (res.status === 200) {
+            setUsername("");
+            setEmail("");
+            setMessage("User created successfully");
+          } else {
+            setMessage("Some error occured");
+          }
+        } catch (err) {
+          console.log(err);
+        }
+      };
 
     return (
         <section>
             <div className = 'register'>
-                <form id='form' className='flex flex-col' onSubmit={handleSubmit(onSubmit)}>
+                <form id='form' className='flex flex-col' onSubmit={handleSubmit}>
                         
                         <h1>Regisztráció</h1>
 
                         <label>Felhasználónév:</label>
-                        <input type="text" {...register("username", {required:true, maxLength:10})}placeholder='Felhasználónév'/>
+                        <input type="text" value={username} {...register("username", {required:true, maxLength:10})}placeholder='Felhasználónév'
+                        onChange={(e) => setUserame(e.target.value)}/>
                          {errors.username?.type ==="required" && "Felhasználónév megadása kötelező"}
                         {errors.username?.type ==="maxLength" && "Felhasználónév túl hosszú"}
                         
                         <label>E-mail:</label>
-                        <input type="text" {...register("email", {required:true,})}placeholder='E-mail' /*onChange={handleChange}*//>
+                        <input type="text" value={email} {...register("email", {required:true,})}placeholder='E-mail' 
+                        onChange={(e) => setEmail(e.target.value)}/>
                         {errors.email?.type ==="required" && "E-mail cím megadása kötelező"}
 
                         <label>Jelszó:</label>
-                        <input type="text" {...register("password", {required:true, maxLength:10})}placeholder='Jelszó'/>
+                        <input type="text" value={password} {...register("password", {required:true, maxLength:10})}placeholder='Jelszó'
+                        onChange={(e) => setPassword(e.target.value)}/>
                         {errors.password?.type ==="required" && "Jelszó megadása kötelező"}
                         {errors.password?.type ==="maxLength" && "Jelszó túl hosszú"}
                        
