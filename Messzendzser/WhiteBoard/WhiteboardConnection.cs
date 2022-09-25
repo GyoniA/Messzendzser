24357f3d2874182c654081786cbb3c07c5c26a1b
GyoniA <gyoniakos@gmail.com>
2022-09-25 13:43:12 +0200

    Start of Whiteboard implementation

---
+++ Messzendzser/WhiteBoard/WhiteboardConnection.cs
@@ -0,0 +1,16 @@
+﻿namespace Messzendzser.WhiteBoard
+{
+    using Messzendzser.Model.DB.Models;
+    using System;
+    using System.IO;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+    using System.Threading;
+    public class WhiteboardConnection
+    {
+        private TcpClient client;
+        private string username;
+        private Chatroom room;
+    }
+}
---
+++ Messzendzser/WhiteBoard/WhiteboardEvent.cs
@@ -0,0 +1,16 @@
+﻿namespace Messzendzser.WhiteBoard
+{
+    public class WhiteboardEvent
+    {
+        private double x;
+        private double y;
+        // TODO change changeType
+        private string changeType;
+
+        public WhiteboardEvent(byte[] message)
+        {
+            // TODO implement
+            throw new NotImplementedException();
+        }
+    }
+}
