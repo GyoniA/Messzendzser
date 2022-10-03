9117d11e2b5deb65a2eacf3707802f943792721f
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2022-10-03 15:05:15 +0200

    messages

---
+++ Messzendzser.Frontend/src/components/ChatApp.js
@@ -0,0 +1,30 @@
+//require('../css/ChatApp.css');
+
+
+import React from 'react';
+import { useNavigate } from "react-router-dom";
+
+import Messages from './Messages';
+import ChatInput from './ChatInput';
+
+// This is where the main logic of the app will be. Here is where we will
+// communicate with the chat server (send and receive messages). We will
+// then pass the data received from the server to other components to be
+// displayed
+class ChatApp extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <h3>Messzendzser</h3>
+        <Messages messages={this.state.messages} />
+        <ChatInput onSend={this.sendHandler} />
+      </div>
+    );
+  }
+
+}
+ChatApp.defaultProps = {
+  username: 'Anonymous'
+};
+
+export default ChatApp;
