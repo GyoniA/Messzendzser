0761a9f3ad9110ef53f272abf994db80a75bec0e
GyoniA <gyoniakos@gmail.com>
2022-10-04 17:37:59 +0200

    Start of text message manager

--- Messzendszer.Model/Managers/Message/MessageManager.cs
+++ Messzendszer.Model/Managers/Message/MessageManager.cs
@@ -9,22 +9,34 @@ namespace Messzendzser.Model.Managers.Message
 {
     public class MessageManager : IMessageManager
     {
+        /// <summary>
+        /// Data source to use for data operations
+        /// </summary>
+        private IDataSource dataSource;
         public MessageManager(IDataSource dataSource)
         {
-
+            this.dataSource = dataSource;
         }
         public void StoreImageMessage(byte[] image, string format, int chatroomId, DB.Models.User user)
         {
+            string token = "";
+            //TODO store image and set token
+            dataSource.StoreImageMessage(user.Id, chatroomId, token, format);
             throw new NotImplementedException();
         }
 
         public void StoreMessage(string message, int chatroomId, DB.Models.User user)
         {
-            throw new NotImplementedException();
+            //TODO check if chatroom and user exists (or don't if it was checked before)
+            dataSource.StoreTextMessage(user.Id, chatroomId, message);
         }
 
         public void StoreVoiceMessage(byte[] sound, string format, int chatroomId, DB.Models.User user)
         {
+            string token = "";
+            int length = 0;
+            //TODO store sound and set token, also set length
+            dataSource.StoreVoiceMessage(user.Id, chatroomId, token, length, format);
             throw new NotImplementedException();
         }
     }
