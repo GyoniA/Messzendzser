b089b6c9369cf5f2cf1087b66cdc3ed34b0166b6
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2022-10-12 21:21:57 +0200

    some progress

--- Messzendzser.Frontend/src/components/Chat.js
+++ Messzendzser.Frontend/src/components/Chat.js
@@ -14,79 +14,97 @@ class Chat extends React.Component {
         this.state = {
             //Here can we load the previous messages
             messages: [],
-            messagesLoaded: false,
-            chatroomsLoade: false,
+
             //This is the new message what we will send
             message: "",
             chatroomId: "",
             error: null
         }
-        //this.handleChange = this.handleChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+        this.create = this.create.bind(this);
     }
 
-    //Get the messages from the API 
+    
+
     //HONNAN TUDJUK MELYIK UZENETEK MERT NEM ADTUK MEG A CHATROOM ID-T------ EZZEL AKKOR MIT KELL TENNI????
     //HOGY NZ KI EGY UZENET? UZENET MELYIK ATTRIBUTUMA A SZOVEG???
     //result.messages? result melyik tagja az uzenetek
     componentDidMount() {
-        fetch("https://localhost:7043/api/GetMessages")
-            .then(res => res.json())
-            .then((result) => {
-                this.setState({
-                    messages: result.messages,
-                    messagesLoaded: true
-                });
-            })
-        //Get chatroomId, and the name in it
-        //Exception handling!!!!!!
-        fetch("https://localhost:7043/api/GetChatrooms")
-            .then(res => res.json())
-            .then((result) => {
-                this.setState({
-                    chatroomId: result.chatroomId,
-                    chatroomsLoaded: true
-                });
-            })
-        //Exception handling!!!!!!
+        //Get the messages from API 
+        fetch("https://localhost:7043/api/GetMessages", {
+            method: "GET",
+            mode: 'cors',
+            headers: {
+            'Access-Control-Allow-Origin': '*'  
+            }
+
+        })
+        .then(res => res.json())
+        .then((res) => {
+            this.setState({
+                messages: res
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+        //Get ChatroomId from API
+        fetch("https://localhost:7043/api/GetChatrooms", {
+            method: "GET",
+            mode: 'cors',
+            headers: {
+                'Access-Control-Allow-Origin': '*'
+            }
+
+        })
+        .then(res => res.json())
+        .then((res) => {
+            this.setState({
+                chatroomId: res
+            });
+        })
+        .catch(err => {
+            console.log(err);
+        });
+
     }
 
-    handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-        let res = await fetch("https://localhost:7043/api/SendMessage", {
+    //Function to send a message to the API
+    create(e) {
+        //POST method
+        e.preventDefault();
+
+        fetch("https://localhost:7043/api/SendMessage", {
             method: "POST",
             mode: 'cors',
             headers: {
                 'Access-Control-Allow-Origin': '*',
                 message: this.state.message,
-                chatroomId: this.state.chatroomId,
+                chatroomId: this.state.chatroomId
             },
-        });
-        let resJson = await res.json();
 
-        if (res.status === 200) {
-            //Here we clear the already sent message
-            this.state.message=("");
-            /*
-            if (resJson.ResponseCode !== 1) {
-                setMessage("Sikeres belépés");
-                setNav(true);
-            } else {
-                let responseM = resJson.Message;
-                setMessage(responseM);
-                setNav(false);
-            }*/
-        }
-    } catch (err) {
-        console.log(err);
+        })
+            .then(res => res.json())
+            .then(res => {
+                console.log(res)
+            })
+            .catch(err => {
+                console.log(err);
+            });
+        
+
+
     }
-};
+    //Function to update inner state
+    handleChange(changeObject) {
+        this.setState(changeObject);
+    }
+
 
 
 
     render() {
-        const { loaded } = this.state.loaded;
-        const { messages } = this.state.messages;
+        
         return (
             <div className='chatapp'>
 
@@ -96,7 +114,7 @@ class Chat extends React.Component {
 
                     <select
                         value={this.state.chatroomId}
-                        onChange={(e) => this.setState({ chatroomId: e.target.value })}
+                        onChange={(e) => this.handleChange({ chatroomId: e.target.value })}
                         >
                         <option>Gábor</option>
                         <option>Gyóni</option>
@@ -119,14 +137,7 @@ class Chat extends React.Component {
  
                 <div className='middle_part'>
                    
-                    <ul>
-                        {messages.map(msg => (
-                            <li key={msg.}>
-                                {msg.}
-                            </li>
-                            
-                        ))}
-                    </ul>
+                    
 
                     <label className='msg_from_me' >
                         Hello
@@ -146,7 +157,11 @@ class Chat extends React.Component {
                 <div className='bottom_row'>
 
                     <button className='send'
-                        onClick={e => this.handleSubmit}>
+                        onClick={(e) => {
+                            this.create(e);
+
+                        }
+                        }>
                         <img src = "/images/send.png" ></img>
                     </button> 
 
@@ -155,7 +170,7 @@ class Chat extends React.Component {
                         type="text"
                         value={this.state.message}
                         placeholder='Üzenet írása...'
-                        onChange={(e) => this.setState({message: e.target.value})}>
+                        onChange={(e) => this.handleChange({ message: e.target.value })}>
                         </input>
                    
 
@@ -173,6 +188,7 @@ class Chat extends React.Component {
 
             </div>
         )
+
     }
 
 }
