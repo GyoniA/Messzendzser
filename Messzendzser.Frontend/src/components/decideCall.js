cf509c88dc372b6204b756fce069479f907a9f2d
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2022-10-15 10:05:26 +0200

    classes

--- Messzendzser.Frontend/src/components/decideCall.js
+++ Messzendzser.Frontend/src/components/decideCall.js
@@ -1,13 +1,18 @@
 import React from "react";
 
-function decideCall() {
+const decideCall= props => {
     return (
         <div className="decide-call">
             <div className="caller">
-                
+                <h3>{props.content}</h3>
             </div>
             <div className="buttons">
-                
+                <button className='pick-up'>
+                    <img src = "/images/pickup.png" ></img>
+                </button>
+                <button className='decline' onClick={props.handleClose}>
+                    <img src = "/images/dropcall.png" ></img>
+                </button>
             </div>
         </div>
     );
