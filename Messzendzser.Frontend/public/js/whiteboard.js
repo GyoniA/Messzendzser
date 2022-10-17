f78e0da42081f8c4775f22374624efa6df2b4d25
GaborHankoczi <30524102+GaborHankoczi@users.noreply.github.com>
2022-10-17 18:46:39 +0200

    Whiteboard authentication message sent

--- Messzendzser.Frontend/public/js/whiteboard.js
+++ Messzendzser.Frontend/public/js/whiteboard.js
@@ -1,38 +1,38 @@
 class WhiteboardManager {
-
-    state = "new";
-    sendAuthMessage = function() {
-        var auth = { "Type": 0, "Username": "Hello", "Password": "World", "ChatroomId": chatroomId };
-        var json = JSON.stringify(auth);
-        this.socket.send(json);
-    }
-
-
-    handleMessage = function (data) {
-        if (state == "new") {
-            if (data.Type == 2) {
-                console.log("authentication succcessful")
-                this.state = "auth"
-            }
-        } else if (this.state == "authenticated") {
-
-        }
-    }
     constructor(canvas, websocketURI, token, chatroomId) {
+        var self = this;
+        this.handleMessage = function (data) {
+            if (state == "new") {
+                if (data.Type == 2) {
+                    console.log("authentication succcessful")
+                    this.state = "auth"
+                }
+            } else if (this.state == "authenticated") {
+
+            }
+        }
+        this.sendAuthMessage = function () {
+            var auth = { "Type": 0, "Username": "Hello", "Password": "World", "ChatroomId": chatroomId };
+            var json = JSON.stringify(auth);
+            self.socket.send(json);
+        }
+
+        this.state = "new";
+
         this.token = token
         this.canvas = canvas
         this.chatroomId = chatroomId
         this.socket = new WebSocket(websocketURI);
         this.socket.onopen = function (event) {
             console.log("Whiteboard connection established")
-            this.sendAuthMessage()
+            self.sendAuthMessage()
         };
         this.socket.onclose = function (event) {
             console.log("Whiteboard connection disconnected")
         };
         this.socket.onmessage = function (event) {
             console.log("Message received: " + event.Data)
-            this.handleMessage(JSON.parse(event.Data))
+            self.handleMessage(JSON.parse(event.Data))
         };
     }
 
