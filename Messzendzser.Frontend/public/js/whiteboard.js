c0a6ce6870649e14b5b64ca14651b78f8aee30af
GaborHankoczi <30524102+GaborHankoczi@users.noreply.github.com>
2022-10-17 19:29:06 +0200

    Whiteboard updates

--- Messzendzser.Frontend/public/js/whiteboard.js
+++ Messzendzser.Frontend/public/js/whiteboard.js
@@ -1,28 +1,52 @@
+class WhiteboardEvent {
+    constructor() {
+        var self = this;
+    }
+}
+
+
+class WhiteboardDotEvent extends WhiteboardEvent {
+    constructor(X,Y,color) {
+        this.Position.X = X 
+        this.Position.Y = X 
+        this.Position.Color = color
+    }
+}
+
+
 class WhiteboardManager {
     constructor(canvas, websocketURI, token, chatroomId) {
         var self = this;
+        this.state = "new";
+        this.socket = new WebSocket(websocketURI);
+
         this.handleMessage = function (data) {
-            if (state == "new") {
+            if (self.state == "new") {
                 if (data.Type == 2) {
                     console.log("authentication succcessful")
-                    this.state = "auth"
+                    self.state = "authenticated"
                 }
             } else if (this.state == "authenticated") {
-
+                if (data.Type == 3) {
+                    var auth = { "Type": 2 };
+                    var json = JSON.stringify(auth);
+                    self.socket.send(json)
+                }
             }
         }
+
+
         this.sendAuthMessage = function () {
             var auth = { "Type": 0, "Username": "Hello", "Password": "World", "ChatroomId": chatroomId };
             var json = JSON.stringify(auth);
             self.socket.send(json);
         }
 
-        this.state = "new";
 
         this.token = token
         this.canvas = canvas
         this.chatroomId = chatroomId
-        this.socket = new WebSocket(websocketURI);
+        
         this.socket.onopen = function (event) {
             console.log("Whiteboard connection established")
             self.sendAuthMessage()
@@ -31,8 +55,8 @@ class WhiteboardManager {
             console.log("Whiteboard connection disconnected")
         };
         this.socket.onmessage = function (event) {
-            console.log("Message received: " + event.Data)
-            self.handleMessage(JSON.parse(event.Data))
+            console.log("Message received: " + event.data)
+            self.handleMessage(JSON.parse(event.data))
         };
     }
 
