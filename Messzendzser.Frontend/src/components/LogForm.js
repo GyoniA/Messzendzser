7557980e62ec07a089971bb69ef32b04c73160eb
kovacsnoi <66200516+kovacsnoi@users.noreply.github.com>
2022-11-16 19:48:15 +0100

    show errors

--- Messzendzser.Frontend/src/components/LogForm.js
+++ Messzendzser.Frontend/src/components/LogForm.js
@@ -1,95 +1,98 @@
-import React, {useState} from 'react'
-import{useForm} from 'react-hook-form';
-import {useNavigate} from "react-router-dom";
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
+import { useNavigate } from "react-router-dom";
 
-export default function Form(){
-  
+export default function Form() {
 
 
-    const{register, formState:{errors}}= useForm()
-    
+
+    const { register, formState: { errors } } = useForm()
+
 
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
 
-    
+
+
+
+    let navigate = useNavigate();
 
     let handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          let res = await fetch("https://localhost:7043/api/Login", {
-            method: "POST",
-            mode: 'cors',
-            
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-                username: username,
-                password: password,
-                
-            },
-          });
-          let resJson = await res.json();
+            let res = await fetch("https://localhost:7043/api/Login", {
+                method: "POST",
+                mode: 'cors',
 
-          if (res.status === 200) {
-            
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    username: username,
+                    password: password,
 
-            if (resJson.message === "Ok") {
-              
-              setUsername("");
-              setPassword("");
-              let tok = resJson.body.token;
-              console.log(tok);
-              document.cookie = "user-token="+resJson.body.token;
+                },
+            });
+            let resJson = await res.json();
 
-              navigate("/chat")
-              
-            } else {
-              let responseM = resJson.Message;
-              setMessage(responseM);
+            if (res.status === 200) {
+
+
+                if (resJson.message === "Ok") {
+
+                    setUsername("");
+                    setPassword("");
+                    let tok = resJson.body.token;
+                    console.log(tok);
+                    document.cookie = "user-token=" + resJson.body.token;
+
+                    navigate("/chat")
+
+                } else {
+                    let responseM = resJson.errors;
+                    setMessage(responseM);
+                }
             }
-          }
         } catch (err) {
-          console.log(err);
+            console.log(err);
         }
-      };
+    };
 
-    
-    let navigate = useNavigate();
 
     return (
         <section>
-            <div className = 'login'>
+            <div className='login'>
                 <form id='form' className='flex flex-col' onSubmit={handleSubmit}>
-                        
-                        <h1>Bejelentkezés</h1>
 
-                        <label>Felhasználónév / e-mail:</label>
-                    <input type="text" value={username} required  placeholder='Felhasználónév / e-mail'
+                    <h1>Bejelentkezés</h1>
+
+                    <label>Felhasználónév / e-mail:</label>
+                    <input type="text" value={username} required placeholder='Felhasználónév / e-mail'
                         onChange={(e) => setUsername(e.target.value)} />
-                         
-                        
-                         
-                        <label>Jelszó:</label>
-                        <input type="password" value={password}  required  placeholder='Jelszó'
-                        onChange={(e) => setPassword(e.target.value)}/>
-                        
 
-                       
-                        
+
+
+                    <label>Jelszó:</label>
+                    <input type="password" value={password} required placeholder='Jelszó'
+                        onChange={(e) => setPassword(e.target.value)} />
+
+
+
+
                     <div className='row'>
-                        
-                            <button type="button" className='reference' 
-                                    onClick={() => {navigate("/register")}}>
-                                    Regisztráció
-                            </button>
 
-                        </div>
+                        <button type="button" className='reference'
+                            onClick={() => { navigate("/register") }}>
+                            Regisztráció
+                        </button>
 
-                        <button type="submit" className='btn'>Belépés</button>
+                    </div>
 
-                        <div className="message">{message ? <p>{message}</p> : null}</div>
-                    </form> 
+                    <button type="submit" className='btn'>Belépés</button>
+
+                    <div className="message">
+                        {message ? <p>{message.username}</p> : null}
+                    </div>
+                </form>
             </div>
         </section>
     )
--- Messzendzser.Frontend/src/components/RegForm.js
+++ Messzendzser.Frontend/src/components/RegForm.js
@@ -1,18 +1,18 @@
-import React, { useState}from 'react'
-import{useForm} from 'react-hook-form';
+import React, { useState } from 'react'
+import { useForm } from 'react-hook-form';
 import { useNavigate } from "react-router-dom";
 
 
 
-export default function Form(){
+export default function Form() {
 
-    const{register,formState:{errors}}= useForm()
+    const { register, formState: { errors } } = useForm()
 
     const [username, setUsername] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const [message, setMessage] = useState("");
-  
+
 
     let navigate = useNavigate();
 
@@ -20,64 +20,70 @@ export default function Form(){
     let handleSubmit = async (e) => {
         e.preventDefault();
         try {
-          let res = await fetch("https://localhost:7043/api/Register", {
-            method: "POST",
-            mode: 'cors',
-              headers: {
-                'Access-Control-Allow-Origin': '*',
-                username: username,
-                email: email,
-                password: password,
-            },
-          });
-          let resJson = await res.json();
-          
-          if (res.status === 200) {
-            
+            let res = await fetch("https://localhost:7043/api/Register", {
+                method: "POST",
+                mode: 'cors',
+                headers: {
+                    'Access-Control-Allow-Origin': '*',
+                    username: username,
+                    email: email,
+                    password: password,
+                },
+            });
+            let resJson = await res.json();
 
-            if (resJson.message === "Ok") {
-                  setUsername("");
-                  setPassword("");
-                  setEmail("");
-                  navigate("/login");
-                  
-              } else {
-                  let responseM = resJson.Message;
-                  setMessage(responseM);
-              }
-            
-          }
+            if (res.status === 200) {
+
+
+                if (resJson.message === "Ok") {
+                    setUsername("");
+                    setPassword("");
+                    setEmail("");
+                    navigate("/login");
+
+                } else {
+                    let responseM = resJson.errors;
+                    setMessage(responseM);
+                }
+
+            }
         } catch (err) {
-          console.log(err);
+            console.log(err);
         }
-      };
+    };
+
+
 
     return (
         <section>
-            <div className = 'register'>
+            <div className='register'>
                 <form id='form' className='flex flex-col' onSubmit={handleSubmit}>
-                        
-                        <h1>Regisztráció</h1>
 
-                        <label>Felhasználónév:</label>
-                    <input type="text" value={username} required  placeholder='Felhasználónév'
-                        onChange={(e) => setUsername(e.target.value)}/>
-                        
-                        <label>E-mail:</label>
-                    <input type="text" value={email} required  placeholder='E-mail' 
-                        onChange={(e) => setEmail(e.target.value)}/>
+                    <h1>Regisztráció</h1>
 
-                        <label>Jelszó:</label>
-                    <input type="password" value={password} required  placeholder='Jelszó'
-                        onChange={(e) => setPassword(e.target.value)}/>
+                    <label>Felhasználónév:</label>
+                    <input type="text" value={username} required placeholder='Felhasználónév'
+                        onChange={(e) => setUsername(e.target.value)} />
 
-                    
+                    <label>E-mail:</label>
+                    <input type="text" value={email} required placeholder='E-mail'
+                        onChange={(e) => setEmail(e.target.value)} />
+
+                    <label>Jelszó:</label>
+                    <input type="password" value={password} required placeholder='Jelszó'
+                        onChange={(e) => setPassword(e.target.value)} />
+
+
 
                     <button type="submit" className='btn'>
-                          Regisztráció</button>
-                          
-                          <div className="message">{message ? <p>{message}</p> : null}</div>
-                    </form> 
+                        Regisztráció</button>
+
+                    <div className="message">
+                        <p>{message.username}</p>
+                        <p>{message.email}</p> 
+                         <p>{message.error}</p>
+                    </div>
+                </form>
             </div>
         </section>
     )
